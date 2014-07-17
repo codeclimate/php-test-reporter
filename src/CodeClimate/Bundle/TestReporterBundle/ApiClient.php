7875a8979f0292a3e540f462a4038be040be8894
patrick brisbin <pbrisbin@gmail.com>
2014-07-17 14:24:22 -0400

    Correctly handle 100 Continue HTTP status
    When the server returns 100 Continue, the full HTTP response will be in
    $response->body. By re-executing the parse on that body until the response is no
    longer 100, we'll get the real status.

--- src/CodeClimate/Bundle/TestReporterBundle/ApiClient.php
+++ src/CodeClimate/Bundle/TestReporterBundle/ApiClient.php
@@ -10,7 +10,7 @@ class ApiClient
         $this->apiHost = "https://codeclimate.com";
 
         if (isset($_SERVER["CODECLIMATE_API_HOST"])) {
-          $this->apiHost = $_SERVER["CODECLIMATE_API_HOST"];
+            $this->apiHost = $_SERVER["CODECLIMATE_API_HOST"];
         }
 
     }
@@ -19,29 +19,37 @@ class ApiClient
     {
         $ch = curl_init();
         curl_setopt_array($ch, array(
-          CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_URL => $this->apiHost.'/test_reports',
-          CURLOPT_USERAGENT => 'Code Climate (PHP Test Reporter v'.Version::VERSION.')',
-          CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
-          CURLOPT_HEADER => true,
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_POSTFIELDS => (string)$json,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_URL => $this->apiHost.'/test_reports',
+            CURLOPT_USERAGENT => 'Code Climate (PHP Test Reporter v'.Version::VERSION.')',
+            CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+            CURLOPT_HEADER => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => (string)$json,
         ));
 
         $response = new \stdClass;
         if ($raw_response = curl_exec($ch)) {
-          list($response->headers, $response->body) = explode("\r\n\r\n", $raw_response, 2);
-          $response->headers = explode("\r\n", $response->headers);
-          list(, $response->code, $response->message) = explode(' ', $response->headers[0], 3);
-        }
-        else {
-          $response->code = -curl_errno($ch);
-          $response->message = curl_error($ch);
-          $response->headers = array();
-          $response->body = NULL;
+            $this->buildResponse($response, $raw_response);
+
+            while ($response->code == 100) { // Continue
+                $this->buildResponse($response, $response->body);
+            }
+        } else {
+            $response->code = -curl_errno($ch);
+            $response->message = curl_error($ch);
+            $response->headers = array();
+            $response->body = NULL;
         }
         curl_close($ch);
 
         return $response;
+    }
+
+    private function buildResponse($response, $body)
+    {
+        list($response->headers, $response->body) = explode("\r\n\r\n", $body, 2);
+        $response->headers = explode("\r\n", $response->headers);
+        list(, $response->code, $response->message) = explode(' ', $response->headers[0], 3);
     }
 }
